41c33e214125937e060439d389d2b3782141818f
thadeus <thadeusnyariki94@gmail.com>
2023-07-27 22:48:07 +0300

    routes added

--- src/App.js
+++ src/App.js
@@ -1,21 +1,57 @@
+import {
+  createBrowserRouter,
+  RouterProvider,
+  Outlet
+} from "react-router-dom";
 import './App.css';
 import 'bootstrap/dist/css/bootstrap.min.css';
-// import About from "./components/About";
+import Home from "./components/Home";
 import Contact from './components/Contact';
-import Navbar from "./components/Navbar"
+// import Navbar from "./components/Navbar"
 
+const Layout = () => {
+  return (
+    <div>
+      {/* <Navbar /> */}
+      <Outlet />
+      {/* <Footer /> */}
+    </div>
+  )
+}
 
-
-
+const router = createBrowserRouter([
+  {
+    path: "/",
+    element: <Layout />,
+    children: [
+      {
+        path: "/",
+        element: <Home />,
+      },
+      {
+        path: "/contact",
+        element: <Contact />,
+      },
+    ]
+  },
+]);
 
 function App() {
   return (
-    <div className="App">
-      <Navbar />
-      {/* <About /> */}
-      <Contact />
+    <div>
+      <RouterProvider router={router} />
     </div>
   );
 }
+
+// function App() {
+//   return (
+//     <div className="App">
+//       <Navbar />
+//       <Home />
+//       <Contact />
+//     </div>
+//   );
+// }
 
 export default App;
