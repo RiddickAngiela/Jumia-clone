765de84a27085a83654f4daffeffcac8aec16179
thadeus <thadeusnyariki94@gmail.com>
2023-07-30 20:56:00 +0300

    created carousel

--- src/components/Home.js
+++ src/components/Home.js
@@ -1,8 +1,19 @@
 import React from 'react'
+import HomeNav from './HomeNav'
+import TopSlider from './TopSlider'
+import ShortCutLinks from './ShortCutLinks'
 
 function Home() {
   return (
-    <div className='home'>Home</div>
+    <div className='home'>
+      <div className='home-container'>
+        <div className='one'>
+          <HomeNav/>
+          <TopSlider/>
+          <ShortCutLinks/>
+        </div>
+      </div>
+    </div>
   )
 }
 
---
+++ src/components/Top.js
@@ -0,0 +1,21 @@
+import React from 'react'
+
+const Top = () => {
+    const top = {
+        phones:'',
+        phones:'',
+        phones:'',
+        phones:'',
+        phones:'',
+        phones:'',
+        phones:'',
+        phones:'',
+        phones:'',
+        phones:'',
+    }
+  return (
+    <div>Top</div>
+  )
+}
+
+export default Top
