4bf4391a348b40cd5de68b0186357190db12ac11
thadeus <thadeusnyariki94@gmail.com>
2023-08-08 22:37:19 +0300

    added account, help and header

--- src/components/Account.jsx
+++ src/components/Account.jsx
@@ -1,7 +1,22 @@
+import { GoPerson } from "react-icons/go";
+import { BiChevronDown } from "react-icons/bi";
+
 const Account = () => {
   return (
-    <div>Account</div>
+    <div style={accountStyle}>
+        <GoPerson/>
+        <span>Account</span>
+        <BiChevronDown/>
+    </div>
   )
+}
+
+const accountStyle={
+    display: 'flex',
+    alignItems: 'center',
+    gap: '10px',
+    cursor: 'pointer',
+    fontSize: '20px'
 }
 
 export default Account
