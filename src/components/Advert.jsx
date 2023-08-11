c01cca23325e859f40fff84956c1223941436167
thadeus <thadeusnyariki94@gmail.com>
2023-08-11 21:44:31 +0300

    :sparkles: feat: Reusable Advert, Icon, NavBar, & Text

---
+++ src/components/Advert.jsx
@@ -0,0 +1,8 @@
+import Image from "./Image"
+import GenericAd from "../assets/genericAd.gif"
+
+const Advert = () => {
+  return <Image image={GenericAd}/>
+}
+
+export default Advert
---
+++ src/components/Text.jsx
@@ -0,0 +1,7 @@
+const Text = ({text, fontSize, color}) => {
+  return (
+    <span style={{fontSize: fontSize, color: color}}>{text}</span>
+  )
+}
+
+export default Text
